b8698b82a31493867b243de16263375c059176b5
safiultarek143 <safiultarek143@gmail.com>
2019-12-22 13:34:50 +0600

    new job project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+
+    <h1 class="text-center">Welcome to Dashboard</h1>
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Admin - Dashboard</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="{{asset('/')}}vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Page level plugin CSS-->
+    <link href="{{asset('/')}}vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="{{asset('/')}}css/sb-admin.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+@if(auth()->check())
+    <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+
+        <a class="navbar-brand mr-1" href="#">Test Project</a>
+
+        <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+            <i class="fas fa-bars"></i>
+        </button>
+
+
+        <!-- Navbar -->
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown no-arrow">
+                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-user-circle fa-fw"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+                </div>
+            </li>
+        </ul>
+
+    </nav>
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="sidebar navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            @if(Auth::user()->user_type_id == 1)
+                <li class="">
+                    <a href="">
+                        <i class="fa fa-laptop"></i>
+                        <span>Profile</span>
+                    </a>
+                </li>
+            @endif
+
+            @if(Auth::user()->user_type_id == 2)
+                <li >
+                    <a href="#">
+                        <i class="fa fa-laptop"></i>
+                        <span>Post New Job</span>
+                    </a>
+                </li>
+            @endif
+
+            <li class="navbar-light">
+                <a class="nav-item -business-time" href="">
+                    <i class="fa fa-search-location"></i>
+                    <span>All Job</span>
+                </a>
+
+            </li>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-fw-"></i>
+                    <span>Products</span>
+                </a>
+            </li>
+        </ul>
+    @endif
+    <!-- /.container-fluid -->
+        <div id="content-wrapper">
+            <div class="container-fluid">
+                @yield('content')
+            </div>
+        </div>
+        <!-- Sticky Footer -->
+    </div>
+
+    <!-- /.content-wrapper -->
+
+    <!-- /#wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout_form').submit()">Logout</a>
+                    <form id="logout_form" action="{{route('logout')}}" method="post">
+                        @csrf
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{asset('/')}}vendor/jquery/jquery.min.js"></script>
+    <script src="{{asset('/')}}vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset('/')}}vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Page level plugin JavaScript-->
+    <script src="{{asset('/')}}vendor/chart.js/Chart.min.js"></script>
+    <script src="{{asset('/')}}vendor/datatables/jquery.dataTables.js"></script>
+    <script src="{{asset('/')}}vendor/datatables/dataTables.bootstrap4.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{asset('/')}}js/sb-admin.min.js"></script>
+
+    <!-- Demo scripts for this page-->
+    <script src="{{asset('/')}}js/demo/datatables-demo.js"></script>
+    <script src="{{asset('/')}}js/demo/chart-area-demo.js"></script>
+
+</body>
+
+</html>
